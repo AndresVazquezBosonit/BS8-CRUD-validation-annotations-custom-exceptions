fc57255ff8d72e54eca2f7010dee8e1f3b015d12
Andrés Vázquez <andres.vazquez@bosonit.com>
2022-06-02 15:02:53 +0200

    Falta exepcion personalizada al hacer un update de persona

---
+++ src/main/java/CRUDvalidacionDTOSmodelMapper/insfrastructure/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package CRUDvalidacionDTOSmodelMapper.insfrastructure.repository;
+
+import CRUDvalidacionDTOSmodelMapper.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+    List<Person> findByName(String name);
+}
